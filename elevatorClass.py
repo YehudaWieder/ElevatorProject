3c933b5d7af1f9bc3e7ff51a0dcc2f63611626e8
mefathim <v4105677@gmail.com>
2025-02-10 00:50:11 +0200

    first commit

--- elevatorClass.py
+++ elevatorClass.py
@@ -10,21 +10,27 @@ class Elevator(pygame.sprite.Sprite):
     def __init__(self, current_floor : int,  pos : (int, int), size : (int, int)):
         super().__init__()
 
-        self.id = len(Elevator.elevators)
-        self.current_floor = current_floor
         self.image = pygame.image.load(ELV_ING_PATH)
         self.image = pygame.transform.scale(self.image, size)
-        # self.rect = self.image.get_rect(midtop=pos)
         self.pos_x = pos[0]
         self.pos_y = pos[1]
         self.tasks = []
         self.tasks_timer = 0
-        self.timer_for_floor = 120
+        self.suspending_for_floor = 120
 
         Elevator.elevators.append(self)
 
     def draw(self, pos, screen):
         screen.blit(self.image, pos)
+
+    def set_suspending_in_floor(self):
+        if self.suspending_for_floor == 120:
+            pygame.mixer.music.play()
+        if self.suspending_for_floor == 0:
+            self.tasks.pop(0)
+            self.suspending_for_floor = 120
+        else:
+            self.suspending_for_floor -= 1
 
     def update(self, delta_time):
         if self.tasks:
@@ -33,29 +39,31 @@ class Elevator(pygame.sprite.Sprite):
             elif self.tasks[0] > self.pos_y:
                 self.pos_y = min(self.pos_y + self.velocity * delta_time, self.tasks[0])
             else:
-                if self.timer_for_floor == 120:
-                    pygame.mixer.music.play()
-                if self.timer_for_floor == 0:
-                    self.tasks.pop(0)
-                    self.timer_for_floor = 120
-                else:
-                    self.timer_for_floor -= 1
+                self.set_suspending_in_floor()
             self.tasks_timer -= 1 / 60
         else:
             self.tasks_timer = 0
 
-    def get_task(self, pos):
+    def get_lest_task(self):
         if self.tasks:
             lest_task = self.tasks[len(self.tasks) - 1]
         else:
             lest_task = self.pos_y
+        return lest_task
 
-        if lest_task > pos[1]:
-            self.tasks_timer += (lest_task - pos[1]) / self.velocity + 2
-        elif lest_task < pos[1]:
-            self.tasks_timer += (pos[1] - lest_task) / self.velocity + 2
+    def get_current_task_time(self, task_pos, lest_task):
+        if lest_task > task_pos:
+            current_task_time = (lest_task - task_pos) / self.velocity
+        elif lest_task < task_pos:
+            current_task_time = (task_pos - lest_task) / self.velocity
+        return current_task_time
 
-        self.tasks.append(pos[1] - ELEVATOR_HEIGHT / 2)
+    def get_new_task(self, task_pos):
+        lest_task = self.get_lest_task()
+        self.tasks.append(task_pos - ELEVATOR_HEIGHT / 2)
+        self.tasks_timer += self.get_current_task_time(task_pos, lest_task) + 2
+
+
 
 
 
